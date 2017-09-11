c0d6cc9c4e5db9b1ab68b8b413ac1426eb15d575
Jessah Marie Duran <duran.jessahmarie@gmail.com>
2017-09-11 19:03:29 +0800

    Removed comments

--- mp1-cost.py
+++ mp1-cost.py
@@ -1,27 +1,20 @@
 #!/usr/bin/python
-
 import numpy as np
+
+x, y, m
 
 def load_data (file):
     dataset = []
     for line in file:
         dataset.append([x.strip() for x in line.split(',')])
-    # convert dataset to a matrix
     mat = np.array(dataset)
-
-    # add a bias column
     dMat = np.ones((mat.shape[0], mat.shape[1]+1), dtype=np.int)
-    dMat[:,1:]=mat
-
-    # load data to respective containers
+    dMat[:,1:] = mat
     x = dMat[:, :-1]
     y = (dMat[:,[dMat.shape[1]-1]])
 
     # one theta example:
     theta = np.matrix('1; 3; 4')
-
-    # get theta
-    # theta = get_theta(x.shape[1]);
 
     # print data
     print ('x: \n', x, '\n')
@@ -32,36 +25,35 @@ def load_data (file):
     cost(x, y, theta)
     return
 
-def get_theta(n):
-    print ('\nSeparate columns using commas (,) and rows using semi-colons (;) \n(i.e.: 1, 2; 3, 4)')
-    theta = np.matrix(input())
-
-    while not (theta.shape[0] == n and theta.shape[1] == 1):
-        print('Not a valid hypothesis')
-        print ('\nSeparate columns using commas (,) and rows using semi-colons (;) \n(i.e.: 1, 2; 3, 4)')
-        theta = np.matrix(input())
-        if (theta.shape[0] == n):
-            return theta
-    return theta
+# def get_theta(n):
+#     print ('\nSeparate columns using commas (,) and rows using semi-colons (;) \n(i.e.: 1, 2; 3, 4)')
+#     theta = np.matrix(input())
+#
+#     while not (theta.shape[0] == n and theta.shape[1] == 1):
+#         print('Not a valid hypothesis')
+#         print ('\nSeparate columns using commas (,) and rows using semi-colons (;) \n(i.e.: 1, 2; 3, 4)')
+#         theta = np.matrix(input())
+#         if (theta.shape[0] == n):
+#             return theta
+#     return theta
 
 def cost(x, y, theta):
     print ("calculating cost...")
-    # cross multiply x and theta
     H = np.matmul(x, theta);
-
-    # subtract y from H
     H_y = (np.subtract(H,y))
-
-    # square the result
     H_ySquared = np.power(H_y, 2, dtype='int64')
-
-    # get the summation of H
     summation = np.sum(H_ySquared)
-
-    # compute cost
     cost = summation / (2 * x.shape[0])
+    m = x.shape[0]
     print ("Cost (J): ", cost)
     return
 
+def gradientDescent(x, y, alpha, iters):
+
+
 file = open("HousePricingRelationship.in", "r")
-load_data(file);
+alpha = 1000
+iters = 100
+
+load_data(file)
+gradientDescent(x, y, alpha, iters)
